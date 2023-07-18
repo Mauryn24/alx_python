64c0db59ec3471c9b43cdf3b861d120f9530069e
Mauryn24 <johndoe@example.com>
2023-07-18 23:53:45 +0300

    decimal numbers

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,2 @@
 for a in range(100):
-    print("{:02d}".format(a), end="")
-    if a < 99:
-        print(", ", end="")
-    else:
-        print()
+    print("{:02d}".format(a), end=", " if i< 99 else "\n")
