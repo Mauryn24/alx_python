59ab533c3a70cd894c2c4a71fe70991a935ee548
Mauryn24 <johndoe@example.com>
2023-07-20 12:50:59 +0300

    prime number

---
+++ python-functions/6-password.py
@@ -0,0 +1,10 @@
+def validate_password(password):
+    if password < 8:
+        return False
+    #check for at least an uppercase, lowercase and digit
+    its_uppercase = any(char.isuper() for char in password)
+    its_lowercase = any(char.islower() for char in password)
+    its_digit = any(char.isdigit() for char in password)
+
+    if not (its_uppercase and its_lowercase and its_digit):
+        return False
