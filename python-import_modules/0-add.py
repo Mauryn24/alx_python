5fec8c25136bc749436ec74c43c6318359c47580
Mauryn24 <johndoe@example.com>
2023-07-24 17:17:42 +0300

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #/usr/bin/python3
-"""In Python, if __name__ == "__main__": is a commonly used construct that allows you to determine whether a Python script is being run as the main program or if it's being imported as a module into another script"""
+
 if __name__ == "__main__":
+    "print the sum of 1 and 2"
     from add_0 import add
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
