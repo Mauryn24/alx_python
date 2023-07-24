a7bd2cd55a943ef7669054ffafe82431549cc818
Mauryn24 <johndoe@example.com>
2023-07-24 17:06:48 +0300

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,9 @@
 #/usr/bin/python3
-def main():
-    a = 1
-    b = 2
-
-    from add_0 import add
-    
-    sum = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
 
 if __name__ == "__main__":
-    main()
+    from add_0 import add
+    a = 1
+    b=2
+
+    sum = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
