7745fea0700a5e94412600dc44d9360c36672116
Mauryn24 <johndoe@example.com>
2023-07-24 17:03:10 +0300

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,12 @@
+#/usr/bin/python3
 def main():
     a = 1
     b = 2
 
-from add_0 import add
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+    from add_0 import add
+    
+    sum = add(a, b)
+    print("{} - {} = {}".format(a, b, sum))
+
 if __name__ == "__main__":
     main()
