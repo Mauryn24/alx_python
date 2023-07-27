a895bc557a3997674f83d58fbfedaff41d43c8f6
Mauryn24 <johndoe@example.com>
2023-07-27 16:09:55 +0300

    common

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,8 +3,9 @@ def common_elements(set_1, set_2):
     c = set(set_2)
 
     #check length
-    if len(b.intersection(c)) > 0:
-        return(b.intersection(c))
+    #if len(b.intersection(c)) > 0:
+     #   return(b.intersection(c))
     
-    #if (b & c):
-     #   print(b & c)
+    #find the common elements using the intersection operation
+    common_elements_set = set_1 & set_2
+    return common_elements_set
