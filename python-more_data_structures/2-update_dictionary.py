24245ec87d8cb6c7e99e8be38886fc60e1793aab
Mauryn24 <johndoe@example.com>
2023-07-27 16:39:55 +0300

    key

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    #if key exists, replace its value with new value
+    if key  in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        #if it does not exist, add new values
+        a_dictionary[key] = value
