df400e52f83184bca85af159d98245e33ddb1d74
Mauryn24 <johndoe@example.com>
2023-07-27 22:40:23 +0300

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        last = row[-1]
+        if last == "":
+            row.pop
         for num in row:
-            print(str.format("{:d}", int(num)), end=" ")
+            print(str.format("{:d}", num), end=" ")
         print()
