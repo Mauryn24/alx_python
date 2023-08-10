0596491ef1f2b287c734acf2869ba24c1b4bc31c
Mauryn24 <johndoe@example.com>
2023-08-10 22:47:48 +0300

    class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
+"""This module defines a class square that finds the square"""
 class Square:
     """creating a class"""
     def __init__(self, size):
-        self._size = size
+        """initializes the square"""
+        self.__size = size
