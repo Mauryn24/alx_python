c7d0d25eb0255ef75013ac3c5f7174f36846b308
Mauryn24 <johndoe@example.com>
2023-08-10 23:17:38 +0300

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+"""This module creates a class square to define square of numbers"""
+class Square:
+    """creating a class"""
+    def __init__(self, size=0):
+        """initializes the square"""
+        self.__size = size
+        @property
+        def size(self):
+            """Gets the size of a square"""
+            return self._size
+        
+        @size.setter
+        def size(self, new_size):
+            """sets the size of the square"""
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self._size = new_size
+    def area(self):
+        """computes the area of a square"""
+        return self.__size * self.__size    
