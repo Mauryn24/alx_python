40b565c6624fb6e0dac00d0b90820251ec9014ea
Mauryn24 <johndoe@example.com>
2023-08-10 23:08:37 +0300

    area

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+"""This module creates a class square to define square of numbers"""
+class Square:
+    """creating a class"""
+    def __init__(self, size=0):
+        """initializes the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """computes the area of a square"""
+        return self.__size * self.__size    
