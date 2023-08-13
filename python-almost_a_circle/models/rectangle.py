11678110d0fc93d503b1b2ec2da1cf30db5ff663
Mauryn24 <johndoe@example.com>
2023-08-13 21:54:49 +0300

    child

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,8 +17,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """set width of a rectangle"""
-        if width < 0:
-            raise ValueError("Width must be non_negative.")
+        if not isinstance(width, int):
+            raise TypeError("Width must be an integer.")
+        if width <= 0:
+            raise ValueError("Width must be > 0.")
         self.__width = width
     
     @property
@@ -29,10 +31,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """set height of the rectangle"""
-        if height < 0:
-            raise ValueError("Height must be a non-negative")
+        if not isinstance(height, int):
+            raise TypeError("Height must be an integer.")
+        if height <= 0:
+            raise ValueError("Height must be > 0.")
         self.__height = height
-
+        
     @property
     def x(self):
         """Get the x-coordinate of the rectangle"""
@@ -41,10 +45,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """Set the x coordinate"""
+        if not isinstance(x, int):
+            raise TypeError("X-coordinate must be an integer.")
         if x < 0:
-            raise ValueError(" X-coordonate must be non-negative")
+            raise ValueError("X-coordinate must be >= 0.")
         self.__x = x
-
+        
     @property
     def y(self):
         """Get the Y-coordinate"""
@@ -53,14 +59,48 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """Set the y-coordinate"""
+        if not isinstance(y, int):
+            raise TypeError("Y-coordinate must be an integer.")
         if y < 0:
-            raise ValueError("Y-coordinate must be a non-negative")
+            raise ValueError("Y-coordinate must be >= 0.")
         self.__y = y
     
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
         super().__init__(id)
+        self.validate_width(width)
+        self.validate_height(height)
+        self.validate_x(x)
+        self.validate_y(y)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
+    def validate_width(self, width):
+        """Validate the width of the rectangle."""
+        if not isinstance(width, int):
+            raise TypeError("Width must be an integer.")
+        if width <= 0:
+            raise ValueError("Width must be > 0.")
+
+    def validate_height(self, height):
+        """Validate the height of the rectangle."""
+        if not isinstance(height, int):
+            raise TypeError("Height must be an integer.")
+        if height <= 0:
+            raise ValueError("Height must be > 0.")
+
+    def validate_x(self, x):
+        """Validate the x-coordinate of the rectangle."""
+        if not isinstance(x, int):
+            raise TypeError("X-coordinate must be an integer.")
+        if x < 0:
+            raise ValueError("X-coordinate must be >= 0.")
+
+    def validate_y(self, y):
+        """Validate the y-coordinate of the rectangle."""
+        if not isinstance(y, int):
+            raise TypeError("Y-coordinate must be an integer.")
+        if y < 0:
+            raise ValueError("Y-coordinate must be >= 0.")
