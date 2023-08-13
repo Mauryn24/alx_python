5deeb3ff7c533d0e303f21572f13deee7c0b785c
Mauryn24 <johndoe@example.com>
2023-08-13 22:00:09 +0300

    child

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,9 +18,9 @@ class Rectangle(Base):
     def width(self, width):
         """set width of a rectangle"""
         if not isinstance(width, int):
-            raise TypeError("Width must be an integer.")
+            raise TypeError("width must be an integer")
         if width <= 0:
-            raise ValueError("Width must be > 0.")
+            raise ValueError("width must be > 0")
         self.__width = width
     
     @property
@@ -32,9 +32,9 @@ class Rectangle(Base):
     def height(self, height):
         """set height of the rectangle"""
         if not isinstance(height, int):
-            raise TypeError("Height must be an integer.")
+            raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError("Height must be > 0.")
+            raise ValueError("height must be > 0")
         self.__height = height
         
     @property
@@ -46,9 +46,9 @@ class Rectangle(Base):
     def x(self, x):
         """Set the x coordinate"""
         if not isinstance(x, int):
-            raise TypeError("X-coordinate must be an integer.")
+            raise TypeError("x must be an integer")
         if x < 0:
-            raise ValueError("X-coordinate must be >= 0.")
+            raise ValueError("x must be >= 0")
         self.__x = x
         
     @property
@@ -60,9 +60,9 @@ class Rectangle(Base):
     def y(self, y):
         """Set the y-coordinate"""
         if not isinstance(y, int):
-            raise TypeError("Y-coordinate must be an integer.")
+            raise TypeError("y must be an integer")
         if y < 0:
-            raise ValueError("Y-coordinate must be >= 0.")
+            raise ValueError("y must be >= 0")
         self.__y = y
     
     def __init__(self, width, height, x=0, y=0, id=None):
