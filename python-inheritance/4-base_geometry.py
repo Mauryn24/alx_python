6d84acb3c397a235fa2758692affdf3e4a1160bb
Mauryn24 <johndoe@example.com>
2023-08-13 10:59:17 +0300

    inheritance

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,5 @@
+"""a module that creates public instance area"""
+class BaseGeometry:
+    """a class that creates a public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
