74569bb60c9191c1399c60e2f0b0dda07bd9ecd5
Mauryn24 <johndoe@example.com>
2023-08-13 21:40:55 +0300

    child

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,5 @@
-"""A module that creates a base class for managing id attributes"""
-class Base:
-    """Base class for managing id attribute in all future classes"""
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Constructor fo Base Class.
-        if id is not None, assign
-        the public instance attribute id
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+"""A module that makes a child class Rectangle"""
+from models.base import Base
 
 class Rectangle(Base):
     """child class Rectangle"""
