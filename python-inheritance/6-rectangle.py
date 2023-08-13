22ac65dbe5b92601a1cdd21a89b339d8327d3954
Mauryn24 <johndoe@example.com>
2023-08-13 11:45:57 +0300

    inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,14 @@
 """a module that create a rectangle which inherits from BaseGeometry"""
-def Rectangle(BaseGeometry):
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
     """class Rectangle which is a child of BaseGeometry"""
     def __init__(self, width, height):
         self.__width = width
