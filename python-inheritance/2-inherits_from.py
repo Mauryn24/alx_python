cbce61f9928fa7a1d00e4d0441bf7e826e64530f
Mauryn24 <johndoe@example.com>
2023-08-13 10:53:43 +0300

    inheritance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,8 +2,4 @@
 def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False."""
     
-    while type(obj) != type(None):
-        if issubclass(type(obj), a_class):
-            return True
-        obj = type(obj)
-    return False 
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
