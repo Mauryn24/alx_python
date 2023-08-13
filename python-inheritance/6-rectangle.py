d8861810491e9462f0c878c700a0cd1a192252c2
Mauryn24 <johndoe@example.com>
2023-08-13 11:15:47 +0300

    inheritance

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,8 @@
+"""a module that create a rectangle which inherits from BaseGeometry"""
+def Rectangle(BaseGeometry):
+    """class Rectangle which is a child of BaseGeometry"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self._width = width
+        self._height = height
