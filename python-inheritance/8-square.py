5811e651657bd6e939e7ec3d4bf7058e90d3d0f8
Mauryn24 <johndoe@example.com>
2023-08-13 11:51:22 +0300

    inheritance

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,28 @@
+"""A module that creates a class Rectangle which is a child of BaseGeometry"""
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
+    """A class Rectangle which is a child of BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    def area(self):
+        return self.__width * self.__height
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+class Square(Rectangle):
+    """Class Square which is a child of Rectangle"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator = ("size", size)
+    def area(self):
+        return self.__size * self.__size
