2700da6d5d9cd6e59d5b93576e1979cb4794f8bd
Mauryn24 <johndoe@example.com>
2023-08-24 22:54:00 +0300

    orm

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,8 @@
 
 """
 a script that takes in arguments and displays all
-values in the states table of hbtn_0e_0_usa where name matches the argument. But this time,
+values in the states table of hbtn_0e_0_usa where
+name matches the argument. But this time,
 write one that is safe from MySQL injections!
 """
 
