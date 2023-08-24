c7686179643d331207cd319f54450bd6f2a8c6e1
Mauryn24 <johndoe@example.com>
2023-08-24 21:42:56 +0300

    list all states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,25 +8,21 @@ in the states table where name matches the arguement
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """connect to the MySQL server"""
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        host='localhost',
-        port=3306)
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
     """create a cursor object"""
     cursor = db.cursor()
 
-    """query"""
-    sql = "SELECT * FROM states\
-        WHERE name LIKE BINARY '{}'\
-        ORDER BR id ASC".format(sys.argv[4])
-
     """execute the query"""
-    cursor.execute(sql)
+    cursor.execute("SELECT * FROM states\
+        WHERE name LIKE BINARY '{}'\
+        ORDER BR id ASC".format(sys.argv[4]))
 
     """fetch the data"""
     data = cursor.fetchall()
