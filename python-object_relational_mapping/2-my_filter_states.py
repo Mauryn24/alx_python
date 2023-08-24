44f3d65216fce0f88087eddc7e98cd7f65b5f1e1
Mauryn24 <johndoe@example.com>
2023-08-24 21:26:27 +0300

    ists all states with a name starting with N

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Takes arguements and displays all values
+in the states table where name matches the arguement
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """connect to the MySQL server"""
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306)
+
+    """create a cursor object"""
+    cursor = db.cursor()
+
+    """execute the query"""
+    cursor.execute("SELECT * FROM states\
+                   WHERE name LIKE BINARY '{}'\
+                   ORDER BR id ASC".format(sys.argv[4]))
+
+    """fetch the data"""
+    data = cursor.fetchall()
+
+    """print the results"""
+    for row in data:
+        print(row)
+
+    """close the cursor"""
+    cursor.close()
+
+    """close the connection"""
+    db.close()
