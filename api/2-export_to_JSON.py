9eb11abbfdff4b0f7b4d277840aac16e0556adab
Mauryn24 <johndoe@example.com>
2023-10-05 22:41:52 +0300

    Dictionary of list of dictionaries

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -13,7 +13,6 @@
 """
 
 
-# Import the necessary libraries
 import json
 import requests
 from sys import argv
@@ -27,43 +26,33 @@ if len(argv) != 2:
 user_id = argv[1]
 
 # Define the endpoint URL to access specific todo items for the user
+url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
 
-# Define the endpoint URL to get specific employee details
-url_user = f"https://jsonplaceholder.typicode.com/users/?id={user_id}"
+# Make an API request to retrieve todo items for the user
+todos_response = requests.get(url_todos)
+todo_data = todos_response.json()
 
-# Make an API request to retrieve user data
-user_data = requests.get(url_user)
-res = user_data.json()
+# Initialize dictionary to store tasks for the user
+user_tasks = {}
 
-for data in res:
+# Iterate through the todo items and store them in the dictionary
+for task in todo_data:
+    task_title = task['title']
+    task_completed = task['completed']
 
-    username = data['username']
+    # Create a dictionary for the task
+    task_dict = {"task": task_title, "completed": task_completed}
 
-    url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    # Append the task to the list of tasks for the user
+    user_tasks[user_id] = task_dict
 
-    # Make an API request to retrieve todo items for the user
-    todos_response = requests.get(url_todos)
-    todo_data = todos_response.json()
+# Read existing data from JSON file
+with open('todo_all_employees.json', 'r') as json_file:
+    all_user_tasks = json.load(json_file)
 
-    # Initialize lists and dictionary
-    data = []
-    user_data = {}
+# Add the user's tasks to the existing data
+all_user_tasks[user_id] = user_tasks
 
-    # Iterate through the todo items and count completed and not completed tasks
-    for x in todo_data:
-        if (x['completed'] == True or x['completed'] == False):
-            tasks = {"task": x['title'],
-                    'completed': x['completed'], 'username': username}
-            data.append(tasks)
-
-    user_data[user_id] = data
-
-    # Serializing JSON
-    json_object = json.dumps(user_data)
-
-    # Create a JSON file using the user id
-    json_file = f'{user_id}.json'
-
-# Writing to a JSON file
-with open(json_file, 'w', encoding='utf-8', newline='') as f:
-    f.write(json_object)
+# Write the updated data to the JSON file
+with open('todo_all_employees.json', 'w') as json_file:
+    json.dump(all_user_tasks, json_file)
