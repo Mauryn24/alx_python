e6e4743e2d202932ac1f2bf41ad9532f2cf3d666
Mauryn24 <johndoe@example.com>
2023-10-05 21:22:50 +0300

    export data in the json format.

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,81 +1,55 @@
 #!/usr/bin/python3
-# This is a shebang line indicating that this script should be run with Python 3.
+""" Export Employee Tasks to JSON
 
+This Python script exports tasks owned by a specific employee in JSON format.
+It retrieves task data for the specified user from a
+remote API and saves it to a JSON file.
+
+Usage:
+    $ python script.py <user_id>
 """
-    A script that gathers data from an API
-    and displays information about a 
-    specific employee's task and exports it to JSON
-"""
-# This is a multi-line string (docstring) that provides a description of what the script does.
-import json      # Import the 'json' module for working with JSON data.
-import requests  # Import the 'requests' module for making HTTP requests.
-from sys import argv  # Import the 'argv' variable from the 'sys' module for handling command-line arguments.
+# Import the necessary libraries
+from sys import argv
+import json
+import requests
 
-# Check for the correct number of command-line arguments
-if len(argv) != 2:
-    print("Usage: python3 gather_data_and_export_to_json.py <employee_id>")
-    exit(1)  # Print a usage message and exit the script if the number of arguments is incorrect.
+# Get the user_id from the command line arguments
+user_id = argv[1]
 
-# Get user_id from the command line arguments
-user_id = argv[1]  # Retrieve the user ID from the command-line argument.
+# Define the endpoint URL to access specific todo items for the user
+url_todos = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
 
-# Define the endpoint URL to access a specific task
-user_todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-# Construct the URL for accessing the specific tasks for the given user ID.
+# Define the endpoint URL to get specific employee details
+url_user = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-# Define the endpoint URL for specific employee details
-user_details_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-# Construct the URL for accessing specific employee details for the given user ID.
+# Make an API request to retrieve user data
+user_data = requests.get(url_user)
+res = user_data.json()
+username = res['username']
 
-try:
-    # Make API requests to retrieve the data
-    user_data = requests.get(user_details_url).json()
-    # Send an HTTP GET request to retrieve user details and parse the JSON response.
+# Make an API request to retrieve todo items for the user
+todos_response = requests.get(url_todos)
+todo_data = todos_response.json()
 
-    # Extract employee username
-    username = user_data['username']
-    # Extract the username from the user_data dictionary.
+# Initialize lists and dictionary
+data = []
+user_data = {}
 
-    # Retrieve the specific tasks for an employee
-    todos_data = requests.get(user_todos_url).json()
-    # Send an HTTP GET request to retrieve task data and parse the JSON response.
+# Iterate through the todo items and count completed and not completed tasks
+for x in todo_data:
+    if (x['completed'] == True or x['completed'] == False):
+        tasks = {"task": x['title'],
+                 'completed': x['completed'], 'username': username}
+        data.append(tasks)
 
-    # Initialize a dictionary to store task records
-    task_records = {}
-    #  create an empty list
-    list = [] 
-    # Create a dictionary with the user ID as the key and an empty list as the value.
+user_data[user_id] = data
 
-    # Iterate through the todo items and create task records
-    for task in todos_data:
-        task_title = task['title']
-        task_completed = task['completed']
+# Serializing JSON
+json_object = json.dumps(user_data)
 
-        # Create a dictionary for each task
-        task_record = {"task": task_title, "completed": task_completed, "username": username}
-        # Create a dictionary with task details in the specified format.
-        list.append(task_record)
-        task_records[user_id] = list
+# Create a JSON file using the user id
+json_file = f'{user_id}.json'
 
-        # task_records[user_id].append(task_record)
-        # Append the task record to the list under the user ID key in the task_records dictionary.
-
-    # Define the JSON file name
-    json_file_name = f'{user_id}.json'
-    # Construct the JSON file name based on the user's ID.
-
-    # Export the data to a JSON file
-    with open(json_file_name, 'w') as jsonfile:
-        #  i need to print data continuously without indentation
-        
-
-        json.dump(task_records, jsonfile)
-    # Write the task_records dictionary to a JSON file with proper formatting.
-
-    print(f"Data has been exported to {json_file_name}")
-    # Print a message indicating that the data has been exported.
-
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
-    exit(1)
-# Handle exceptions related to HTTP requests and print an error message.
+# Writing to a JSON file
+with open(json_file, 'w', encoding='utf-8', newline='') as f:
+    f.write(json_object)
