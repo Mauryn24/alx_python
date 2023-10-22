8beede383f0683838fc9d738acf746063b7cd647
Mauryn24 <johndoe@example.com>
2023-10-22 17:19:56 +0300

    prints the number of and the list of its arguments.

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 import sys
 
-def get_arguments(argv):
-    # get number of args passed
-    num_arguments = len(argv) - 1
-    plural = "s" if num_arguments != 1 else ""
 
-    #printnumber of args
-    print("{:d} argument{}{}".format(num_arguments, plural, ":" if num_arguments > 0 else "", end=""))
-    #check if there are no args
-    if num_arguments == 0:
-        print(" 0 arguments.")
+def print_arguments(*argv):
+    """prints the number of and the list of its arguments"""
+    # count how many args were passed in,
+    #  starting from 1 (not including script name)
+    i = 0
+    length = len(sys.argv) - 1
+    if length == 1:
+        print("{:d} argument:".format(length))
+    elif length == 0:
+        print("{:d} arguments.".format(length))
     else:
-        #print()
-    #loop thru and prnt args with their positions
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
-#callthe get args with args passed
+        print("{:d} arguments:".format(length))
+    # loop through all the system arguments passed
+    for args in sys.argv:
+        if i != 0:
+            print("{}: {}".format(i, args))
+        i += 1
+
+
 if __name__ == "__main__":
-    get_arguments(sys.argv)
+    print_arguments()
