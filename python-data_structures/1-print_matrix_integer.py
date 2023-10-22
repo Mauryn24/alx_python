364b550395ea0cd365ecd56fd05e3d67a3e3fb17
Mauryn24 <johndoe@example.com>
2023-10-22 17:23:01 +0300

    prints a matrix of integers.

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
+    # Check if the matrix is empty
+    if not matrix:
+        print()
+        return
+
+    # Loop through each row in the matrix
     for row in matrix:
-        last = row[-1]
-        if last == "":
-            row.pop
-        for num in row:
-            print(str.format("{:d}", num), end=" ")
+        # Loop through each element in the row
+        for i, element in enumerate(row):
+            # Print the element with proper formatting
+            print("{:d}".format(element), end="")
+            
+            # Print a space unless it's the last element in the row
+            if i < len(row) - 1:
+                print(" ", end="")
+        
+        # Move to the next line after printing all elements in the row
         print()
